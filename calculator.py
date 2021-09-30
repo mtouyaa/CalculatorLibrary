7fe8c2ff5831d7f7dec7068f3af693effdfa1fbc
Manuel <manuel.touyaa@gmail.com>
2021-09-30 08:05:19 +0100

    Add functions add and subst

---
+++ calculator.py
@@ -0,0 +1,9 @@
+'''
+Calculator library containing basic math operations
+'''
+
+def add(first_item, second_item):
+    return( first_item + second_item )
+
+def substract(first_item, second_item):
+    return( first_item - second_item )
