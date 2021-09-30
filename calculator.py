c7a7d5cef0f8642adf98d01f1845eed214b10dd0
Manuel <manuel.touyaa@gmail.com>
2021-09-30 19:18:10 +0100

    commit to 'main'

--- calculator.py
+++ calculator.py
@@ -13,4 +13,4 @@ def substract(first_item, second_item):
 
 def multiply(first_item, second_item):
     return(first_item * second_item) 
-    # spaces before & after * are required, due to linter checks
+#  spaces before & after * are required, due to linter checks
